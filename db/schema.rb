1ca2ecfcfc5e6c8b373078a028961a1de77ea997
Agnieszka Stec <agnieszkaa.stec@gmail.com>
2019-09-30 14:31:28 +0200

    db migrate models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_30_111248) do
+ActiveRecord::Schema.define(version: 2019_09_30_121707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "name"
+    t.integer "number"
+    t.integer "balance"
+    t.string "bank"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "color"
+    t.string "name"
+    t.string "priority"
+    t.bigint "user_id"
+    t.bigint "icon_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["icon_id"], name: "index_categories_on_icon_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "icons", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.datetime "datetime"
+    t.integer "amount"
+    t.string "store"
+    t.string "address"
+    t.datetime "approved_at"
+    t.bigint "account_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_transactions_on_account_id"
+    t.index ["category_id"], name: "index_transactions_on_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +71,9 @@ ActiveRecord::Schema.define(version: 2019_09_30_111248) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accounts", "users"
+  add_foreign_key "categories", "icons"
+  add_foreign_key "categories", "users"
+  add_foreign_key "transactions", "accounts"
+  add_foreign_key "transactions", "categories"
 end
