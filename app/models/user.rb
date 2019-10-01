107dc0f42abd40d47cea2c8614d4a05b0250db0b
Jostein Hamre <josteinhamre@gmail.com>
2019-10-01 18:23:14 +0200

    adde category icons to budget page

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
       Category.create(user: self, name: 'Travel', icon: (Icon.find_by(name: 'Travel')), color: '#ED6237')
       Category.create(user: self, name: 'Rent & Loans', icon: (Icon.find_by(name: 'Rent & Loans')), color: '#9E51B6')
       Category.create(user: self, name: 'Sports & Activities', icon: (Icon.find_by(name: 'Sports & Activities')), color: '#414141')
-      Category.create(user: self, name: 'Tra nsport', icon: (Icon.find_by(name: 'Transport')), color: '#37818D')
+      Category.create(user: self, name: 'Transport', icon: (Icon.find_by(name: 'Transport')), color: '#37818D')
       Category.create(user: self, name: 'Savings', icon: (Icon.find_by(name: 'Savings')), color: '#419388')
       Category.create(user: self, name: 'Clothing', icon: (Icon.find_by(name: 'Clothing')), color: '#9E9E9E')
       Category.create(user: self, name: 'Bills & Fees', icon: (Icon.find_by(name: 'Bills & Fees')), color: '#67DA21')
