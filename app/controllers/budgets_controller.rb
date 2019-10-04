c4c3e719718ee51371b400ec19b80c92298f851a
Jostein Hamre <josteinhamre@gmail.com>
2019-10-04 11:49:43 +0200

    Budgets create and update working.

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,10 +1,17 @@
 class BudgetsController < ApplicationController
-def create
-  @budget = Budget.new
-  @budget.amount = params['budget[amount]']
-  @budget.month_from = params['budget']['month_from']
-  @budget.category = Category.find(params['budget']['category'])
-  @budget.save!
-  p @budget
-end
+  def create
+    @budget = Budget.new
+    @budget.amount = params['budget']['amount']
+    @budget.month_from = Date.parse("1 + #{params['budget']['month_from']}")
+    @budget.category = Category.find(params['budget']['category'])
+    @budget.save!
+    p @budget
+  end
+
+  def update
+    @budget = Budget.find(params['id'])
+    @budget.amount = params['budget']['amount']
+    @budget.save!
+    p @budget
+  end
 end
