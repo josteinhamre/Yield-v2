e9d826d666b7c0fb16b5dde5bc057857840bb4c0
Jostein Hamre <josteinhamre@gmail.com>
2019-10-04 14:13:51 +0200

    Added info to budget

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -5,13 +5,22 @@ class BudgetsController < ApplicationController
     @budget.month_from = Date.parse("1 + #{params['budget']['month_from']}")
     @budget.category = Category.find(params['budget']['category'])
     @budget.save!
-    p @budget
+    get_transactions
   end
 
   def update
     @budget = Budget.find(params['id'])
     @budget.amount = params['budget']['amount']
     @budget.save!
-    p @budget
+    get_transactions
+  end
+
+  def get_transactions
+    sql_query = " \
+        extract(month from datetime) = ? \
+        AND extract(year from datetime) = ? \
+      "
+    date = Date.parse("1 #{@selected_month}")
+    @transactions = current_user.transactions.where(sql_query, date.month, date.year)
   end
 end
