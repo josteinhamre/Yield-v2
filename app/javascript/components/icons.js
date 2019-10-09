3011766d5bc3ac1369ad694238f770f66622aa2f
Agnieszka Stec <agnieszkaa.stec@gmail.com>
2019-10-09 15:57:48 +0200

    adding categories

--- app/javascript/components/icons.js
+++ app/javascript/components/icons.js
@@ -1,27 +1,20 @@
 const setupProfileIcons = () => {
   document.querySelector('.category-container').addEventListener('click', event => {
-    console.log(event.target)
-    // if (event.target('icon-container')) {
-    //   console.log('icon clicked');
-    // }
-  })
+    if (event.target.tagName !== 'IMG') {
+      return;
+    }
 
+    event.preventDefault();
 
-  // let icons = document.querySelectorAll(".icons");
-  // icons.forEach(icon => {
-  //   icon.addEventListener("click", (event) => {
-  //     //event.preventDefault();
+    // deselect other icons
+    document.querySelectorAll('.icon-container').forEach(icon => {
+      icon.classList.remove('selected');
+    });
 
-  //     // deselect other icons
-  //     document.querySelectorAll('.icon-container').forEach(icon => {
-  //       icon.classList.remove('selected');
-  //     })
-
-  //     // set id on form
-  //     document.getElementById('category_icon_id').value = event.target.parentElement.dataset.id;
-  //     event.target.parentElement.classList.add('selected')
-  //   });
-  // });
+    // set id on form
+    document.getElementById('category_icon_id').value = event.target.parentElement.dataset.id;
+    event.target.parentElement.classList.add('selected');
+  });
 };
 
 export { setupProfileIcons };
