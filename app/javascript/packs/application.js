a6c88163436499f8b139b4cc3d8f6d23e68947cf
Agnieszka Stec <agnieszkaa.stec@gmail.com>
2019-10-09 09:02:34 +0200

    conflicts solved

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,8 @@
 import "bootstrap";
 import { listOpen } from '../components/chevron_menu';
 import { submitOn } from '../components/submit_on';
-<<<<<<< HEAD
 import { setupProfileIcons } from '../components/icons';
-// import { changeMonth } from '../components/month_selector';
-=======
 
->>>>>>> master
-
-// changeMonth()
 submitOn();
 listOpen();
 setupProfileIcons();
