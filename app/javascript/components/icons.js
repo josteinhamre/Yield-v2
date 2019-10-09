3ab7b38c0fee6e3df4111617b564c9fba5cf5879
Agnieszka Stec <agnieszkaa.stec@gmail.com>
2019-10-09 15:13:36 +0200

    profile page looks ok

--- app/javascript/components/icons.js
+++ app/javascript/components/icons.js
@@ -1,12 +1,27 @@
 const setupProfileIcons = () => {
-  const icons = document.querySelectorAll(".icons");
-  icons.forEach(icon => {
-    icon.addEventListener("click", (event) => {
-      event.preventDefault();
-      console.log(event.target.parentElement.dataset.id)
-      document.getElementById('category_icon_id').value = event.target.parentElement.dataset.id
-    });
-  });
+  document.querySelector('.category-container').addEventListener('click', event => {
+    console.log(event.target)
+    // if (event.target('icon-container')) {
+    //   console.log('icon clicked');
+    // }
+  })
+
+
+  // let icons = document.querySelectorAll(".icons");
+  // icons.forEach(icon => {
+  //   icon.addEventListener("click", (event) => {
+  //     //event.preventDefault();
+
+  //     // deselect other icons
+  //     document.querySelectorAll('.icon-container').forEach(icon => {
+  //       icon.classList.remove('selected');
+  //     })
+
+  //     // set id on form
+  //     document.getElementById('category_icon_id').value = event.target.parentElement.dataset.id;
+  //     event.target.parentElement.classList.add('selected')
+  //   });
+  // });
 };
 
 export { setupProfileIcons };
